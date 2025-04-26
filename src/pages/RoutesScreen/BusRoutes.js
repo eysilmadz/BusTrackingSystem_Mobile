a0bbbeaa854478c35ee389b7f8c700294fb01888
Esma Yıldız <esma.yildiz2@ogr.sakarya.edu.tr>
2025-04-26 23:24:33 +0300

    Fetch routes based on selected city from backend for routes screen

--- src/pages/RoutesScreen/BusRoutes.js
+++ src/pages/RoutesScreen/BusRoutes.js
@@ -3,32 +3,26 @@ import { View, Text, TouchableOpacity, FlatList, SafeAreaView } from "react-nati
 import Icon from 'react-native-vector-icons/Ionicons';
 import styles from "./BusRoutes.style";
 import { useNavigation } from '@react-navigation/native';
-import axios from "axios";
-import { API_URL } from '@env';
 import { useGlobalContext } from "../../contexts/GlobalContext";
 import { useFavouriteContext } from "../../contexts/FavouriteContext";
+import { getRoutesByCityId } from "../../api/routeService";
 
 const BusRoutes = ({ route }) => {
   const selectedCity = route.params.city;
+  const cityId = route.params.cityId;
   const location = route.params.location;
+
   const [busRoutes, setBusRoutes] = useState([]);
   const { favouriteRoutes, toggleRouteFavourite } = useFavouriteContext();
   const { setLoading, setError, setErrorWithCode } = useGlobalContext();
   const navigation = useNavigation();
 
   const fetchData = async () => {
-    if (!selectedCity) return;
+    if (!cityId) return;
     try {
-      const response = await axios.get(`${API_URL}/cities`);
-      console.log(response);
-      const data = response.data;
-      const city = data.find((city) => city.cityName === selectedCity);
-      if (city && city.routes) {
-        const routes = city.routes;
-        setBusRoutes(routes);
-      } else {
-        setBusRoutes([]);
-      }
+      setLoading(true);
+      const routes = await getRoutesByCityId(cityId);
+      setBusRoutes(routes);
     } catch (error) {
       console.log(error)
       console.error("Error fetching data(BusRoutes.js):", error);
@@ -37,6 +31,7 @@ const BusRoutes = ({ route }) => {
       }
       setBusRoutes([]);
     } finally {
+      //setLoading(false);
     }
   };
 
@@ -44,27 +39,31 @@ const BusRoutes = ({ route }) => {
     fetchData();
   }, []);
 
+  const renderRouteItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => navigation.navigate('RoutesDetail', { routes: item, city: selectedCity, location: location })}
+    >
+      <Icon name="bus-outline" size={28} color="#666" style={styles.icon} />
+      <View style={styles.cardContent}>
+        <Text style={styles.routeName}>{item.routeName}</Text>
+        <Text style={styles.routeLine}>{item.routeLine}</Text>
+      </View>
+      <TouchableOpacity onPress={() => toggleRouteFavourite(item.routeId)} style={styles.favouriteIcon}>
+        <Icon
+          name={favouriteRoutes.includes(item.routeId) ? "heart" : "heart-outline"}
+          size={28}
+          color={favouriteRoutes.includes(item.routeId) ? "#222" : "#666"}
+        />
+      </TouchableOpacity>
+    </TouchableOpacity>
+  );
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={busRoutes}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <TouchableOpacity key={route.routeId} style={styles.card} onPress={() => navigation.navigate('RoutesDetail', { routes: item, city: selectedCity, location: location })}>
-            <Icon name="bus-outline" size={28} color="#666" style={styles.icon} />
-            <View style={styles.cardContent}>
-              <Text style={styles.routeName}>{item.routeName}</Text>
-              <Text style={styles.routeLine}>{item.routeLine}</Text>
-            </View>
-            <TouchableOpacity onPress={() => toggleRouteFavourite(item.routeId)} style={styles.favouriteIcon}>
-              <Icon
-                name={favouriteRoutes.includes(item.routeId) ? "heart" : "heart-outline"}
-                size={28}
-                color={favouriteRoutes.includes(item.routeId) ? "#222" : "#666"}
-              />
-            </TouchableOpacity>
-          </TouchableOpacity>
-        )}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderRouteItem}
       >
       </FlatList>
     </SafeAreaView>
